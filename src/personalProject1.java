eb2b3d0d239abb6a3448eca9d1c3dcb2ac7431d2
Nikolaos Moraru <bignickmax@gmail.com>
2023-06-04 15:22:13 +0300

    Calculator

---
+++ src/personalProject1.java
@@ -0,0 +1,91 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class personalProject1 {
+    public static void main(String[] args) {
+        boolean statement;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double number1 = 0;
+
+        do {
+            try {
+                statement = false;
+                number1 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                statement = true;
+                System.out.println("Please enter a number");
+                scanner.next();
+            }
+        }
+        while (statement);
+
+        System.out.println("Enter the operation");
+        char operation;
+
+        do {
+            operation = scanner.next().charAt(0);
+            if (operation != '/' && operation != '*' && operation != '-' && operation != '+') {
+                statement = true;
+                System.out.println("Please enter an operation");
+            } else {
+                statement = false;
+            }
+
+        } while (statement);
+        System.out.println("Enter the second number");
+        double number2 = 0;
+
+        do {
+            try {
+                statement = false;
+                number2 = scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                statement = true;
+                System.out.println("Enter a number");
+                scanner.next();
+            }
+        } while (statement);
+
+        while (true) {
+            switch (operation) {
+                case '/':
+                    division(number1, number2);
+                    return;
+                case '*':
+                    multiplication(number1, number2);
+                    return;
+                case '-':
+                    substraction(number1, number2);
+                    return;
+                case '+':
+                    addition(number1, number2);
+                    return;
+            }
+        }
+    }
+
+
+    public static void addition(double number1, double number2) {
+
+        double addition = number1 + number2;
+        System.out.println(addition);
+    }
+
+    public static void division(double number1, double number2) {
+        double division = number1 / number2;
+        System.out.println(division);
+    }
+
+    public static void substraction(double number1, double number2) {
+        double substraction = number1 - number2;
+        System.out.println(substraction);
+    }
+
+    public static double multiplication(double number1, double number2) {
+        double multiplication = number1 * number2;
+        System.out.println(multiplication);
+        return multiplication;
+    }
+
+}
